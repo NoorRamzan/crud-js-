ccfc7a6bb37feb49cee0f87dfa790665cac61ddc
Noor Ramzan <NoorRamzan002@gmail.com>
2024-11-12 23:00:12 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import About from './About';
+import './App.css';
+import {Routes,Route} from 'router-react-DOM'
+
+function App() {
+  return (
+  
+
+  <>
+    <home.js/>
+  
+
+  </>
+  ); 
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='text-center'>
+      <h1>home</h1>
+    </div>
+  )
+}
+
+export default home
